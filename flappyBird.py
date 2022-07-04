b27d276d1fb3c6f59bafeb3119e07b88d19c00b9
Assis Araujo <assissousa207@gmail.com>
2022-07-04 14:35:59 -0400

    desenhar imagem

--- flappyBird.py
+++ flappyBird.py
@@ -1,16 +1,13 @@
 import pygame
 import os
 import random
-
 import self as self
 
 TELA_LARGURA = 500
 TELA_ALTURA = 800
-
 IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
 IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','base.png')))
 IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bg.png')))
-
 IMAGEM_PASSARO = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird1.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),
@@ -19,8 +16,6 @@ IMAGEM_PASSARO = [
 
 pygame.font.init()
 FONTE_PONTOS = pygame.font.SysFont('ariel', 50)
-
-
 class Passaro:
     IMGS = IMAGEM_PASSARO
     #animaçõe da rotação
@@ -39,13 +34,12 @@ class Passaro:
         self.contagem_imagem = 0
         self.imagem = self.IMGS [0]
 
-        def pular(self):
+    def pular(self):
+         self.velocidade = -10.5
+         self.tempo = 0
+         self.altura = self.y
 
-            self.velocidade = -10.5
-            self.tempo = 0
-            self.altura = self.y
-
-        def mover(self):
+    def mover(self):
             #calcular o deslocamento
             self.tempo += 1
             deslocamento = 1.5 * (self.tempo**2)  + self.velocidade * self.tempo
@@ -66,10 +60,9 @@ class Passaro:
                     if self.angulo > -90:
                         self.angulo -= self.VELOCIDADE_ROTACAO
 
-        def desenhar(self, tela ):
+    def desenhar(self, tela):
 
         # definir qual imagem do passaro vai usar
-
         self.contagem_imagem += 1
 
         if self.contagem_imagem < self.TEMPO_ANIMACAO:
@@ -80,20 +73,20 @@ class Passaro:
             self.imagem = self.IMGS[2]
         elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
             self.imagem = self.IMGS[1]
-        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 +1:
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 +  1:
             self.imagem = self.IMGS[0]
             self.contagem_imagem = 0
 
-    # se o passaro tiver caindo ele não vai bater a asa
+        # se o passaro tiver caindo ele não vai bater a asa
         if self.angulo <= -80:
             self.imagem = self.IMGS[1]
-        self.contagem_imagem = self.TEMPO_ANIMACAO*2
+            self.contagem_imagem = self.TEMPO_ANIMACAO*2
 
-    # desenhar a imagem
-    imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
-    pos_centro_imagem = self.imagem.get_ect(topleft=(self.x, self.y)).center
-    retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
-    tela.blit(imagem_rotacionada, retangulo.topleft)
+       # desenhar a imagem
+       imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+       pos_centro_imagem = self.imagem.get_ect(topleft=(self.x, self.y)).center
+       retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
+       tela.blit(imagem_rotacionada, retangulo.topleft)
 
 class Cano:
     pass
